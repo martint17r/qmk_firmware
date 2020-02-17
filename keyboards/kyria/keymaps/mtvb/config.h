edae4feb3ef239ee294dac236fdd38cbf84f0350
Martin Treusch von Buttlar <martin@t17r.de>
2020-02-17 09:34:00 +0100

    update

--- keyboards/kyria/keymaps/mtvb/config.h
+++ keyboards/kyria/keymaps/mtvb/config.h
@@ -28,5 +28,7 @@
 #endif
 
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
+#define SPLIT_USB_DETECT
+#define NO_USB_STARTUP_CHECK
+
+#define TAP_CODE_DELAY 100
